f1bc32a9db21624e02ab75e71242b048ff8c1f11
Klaudia0212 <klaudia.scigalska@gmail.com>
2026-06-13 15:24:09 +0200

    zrobiona klasa EmployeeReport 2

--- src/main/java/com/company/enroller/service/EmployeeReport.java
+++ src/main/java/com/company/enroller/service/EmployeeReport.java
@@ -1,6 +1,30 @@
 package com.company.enroller.service;
 
-public class EmployeeReport implements Report {
+import com.company.enroller.model.DataReport;
+import com.company.enroller.model.Records;
+
+import java.util.List;
+
+public class EmployeeReport extends Report {
+
+    private final DataReport dataReport;
+
+    public EmployeeReport() {
+        this.dataReport = new DataReport(
+                "Raport 1 - Czas pracy pracowników przy projektach",
+                "Raport przedstawia liczbę godzin przepracowanych przez pracowników przy konkretnych projektach.",
+                List.of("Imię i nazwisko", "Godziny", "Projekt"),
+                "employee_report.xlsx"
+        );
+    }
+
     @Override
-    public void generateReport() {}
+    public void generateReport(Records records) {
+
+    }
+
+    @Override
+    public DataReport getReport() {
+        return dataReport;
+    }
 }
