291c9bb08093fce686476790744542ac057e91b7
Student <student@d17-427-pc-16.ipa.ii.agh.edu.pl>
2026-06-13 15:37:11 +0200

    dodanie TaskReport

--- src/main/java/com/company/enroller/service/TaskReport.java
+++ src/main/java/com/company/enroller/service/TaskReport.java
@@ -3,9 +3,28 @@ package com.company.enroller.service;
 import com.company.enroller.model.DataReport;
 import com.company.enroller.model.Records;
 
+import java.util.List;
+
 public class TaskReport extends Report {
+
+    private final DataReport dataReport;
+
+    public TaskReport() {
+        this.dataReport = new DataReport(
+                "Raport 4 - Zadania zajmujące najwięcej czasu",
+                "Raport przedstawia listę zadań posortowaną według liczby przepracowanych godzin.",
+                List.of("Godziny", "Projekt", "Zadanie"),
+                "task_report.xlsx"
+        );
+    }
+
     @Override
     public void generateReport(Records records) {
+        System.out.println("Generowanie raportu zadań...");
+    }
 
+    @Override
+    public DataReport getReport() {
+        return dataReport;
     }
 }
