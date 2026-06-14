95e5a33f3d438987b83cbf613f0feb9e54bbe686
Lila <lila.88.xx@gmail.com>
2026-06-14 10:57:27 +0200

    zaczytanie i zapis danych z pozostałych zakładek excela

--- src/main/java/com/company/managementSystem/App.java
+++ src/main/java/com/company/managementSystem/App.java
@@ -1,12 +1,13 @@
 package com.company.managementSystem;
 
-import com.company.managementSystem.model.DataReport;
+import com.company.managementSystem.persistence.DatabaseConnector;
 import com.company.managementSystem.presentation.ConsolePrinter;
 import com.company.managementSystem.presentation.Printer;
-import com.company.managementSystem.service.*;
-import com.company.managementSystem.persistence.DatabaseConnector;
+import com.company.managementSystem.service.EmployeeReport;
+import com.company.managementSystem.service.ProjectReport;
+import com.company.managementSystem.service.ReadExcel;
+import com.company.managementSystem.service.TaskReport;
 import org.hibernate.Session;
-
 
 import java.io.IOException;
 
@@ -16,53 +17,67 @@ public class App {
 
     public static void main(String[] args) throws IOException {
 
-        boolean runReadExcel = false;
+        boolean runReadExcel = true;
         boolean runReadPdf = false;
         boolean runEmployeeReport = true;
         boolean runProjectReport = false;
         boolean runTaskReport = false;
-        Printer printer = new ConsolePrinter();;
-
+        Printer printer = new ConsolePrinter();
 
         //String filePath = args[0];
 
-        for(String arg : args){
-            if(arg.equals("-g")){runReadExcel = true;}
-            if(arg.equals("-rEmp")){runEmployeeReport = true;}
-            if(arg.equals("-rProj")){runProjectReport = true;}
-            if(arg.equals("-rTask")){runTaskReport = true;}
-            if(arg.startsWith("-rTyp=")){
-                switch(arg.substring(6)){
-                    case "PDF": printer = new ConsolePrinter(); break;
-                    case "XML": printer = new ConsolePrinter(); break;
-                    case "CSV": printer = new ConsolePrinter(); break;
-                    case "Con": printer = new ConsolePrinter(); break;
+        for (String arg : args) {
+            if (arg.equals("-g")) {
+                runReadExcel = true;
+            }
+            if (arg.equals("-rEmp")) {
+                runEmployeeReport = true;
+            }
+            if (arg.equals("-rProj")) {
+                runProjectReport = true;
+            }
+            if (arg.equals("-rTask")) {
+                runTaskReport = true;
+            }
+            if (arg.startsWith("-rTyp=")) {
+                switch (arg.substring(6)) {
+                    case "PDF":
+                        printer = new ConsolePrinter();
+                        break;
+                    case "XML":
+                        printer = new ConsolePrinter();
+                        break;
+                    case "CSV":
+                        printer = new ConsolePrinter();
+                        break;
+                    case "Con":
+                        printer = new ConsolePrinter();
+                        break;
                 }
             }
         }
 
         Session session = DatabaseConnector.getInstance().getSession();
 
-        if(runReadExcel){
+        if (runReadExcel) {
             new ReadExcel(session).run();
         }
 
-        if(runReadPdf){
+      /*  if(runReadPdf){
             new ReadPdf(session).run();
-        }
+        }*/
 
-        if(runEmployeeReport){
+        if (runEmployeeReport) {
             printer.printReport(new EmployeeReport(session).generateReport());
         }
 
-        if(runProjectReport){
+        if (runProjectReport) {
             printer.printReport(new ProjectReport(session).generateReport());
         }
 
-        if(runTaskReport){
+        if (runTaskReport) {
             printer.printReport(new TaskReport(session).generateReport());
         }
-
 
         DatabaseConnector.getInstance().teardown();
     }
